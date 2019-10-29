3399a5dff1e32eecc4390c4f0b44e163ea17730e
Luke <deandrevidal@aol.com>
2019-10-29 11:21:18 -0700

    started code

---
+++ poetry.py
@@ -0,0 +1,28 @@
+poem = """Add a poem here """
+
+
+def lines_printed_backwards(lines_list):
+    # TODO: reverse the list
+    # lines_list.reverse()
+    # print(lines_list)
+    # TODO: Print items from list
+    pass
+
+
+def lines_printed_random():
+    pass
+
+
+def my_custom_function():
+    pass
+
+
+"""TODO: Stretch challanges 
+Modify your program to read the poem from a file
+Modify your program to read the poem from user input
+Modify your program to randomly rearrange the words on each line"""
+
+
+def test_poem():
+    # TODO: test code here
+    pass
